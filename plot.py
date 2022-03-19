1098a034753d4199b4e61fe270a152f626ead5f8
rayray2002 <rayra2002.huang@gmail.com>
2022-03-19 08:24:21 +0000

    rpi

--- plot.py
+++ plot.py
@@ -5,9 +5,9 @@ pd.set_option('display.max_columns', 500)
 
 users = ["A", "B", "C", "D", "E"]
 for user in users:
-    df = pd.read_csv(f'data/{user}_SM.csv')
+    df = pd.read_csv(f'data/{user}_out.csv')
 
     cols = df.columns
-    df.plot.line(x='time', subplots=True, figsize=(24,8))
+    df.plot.line(x='timestamp', subplots=True, figsize=(24,8))
     plt.title(user)
     plt.savefig(f'data/plot/{user}_SM.png')

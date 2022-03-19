8559f51c0d5ba80bf1bcd3eb2499162c977abd1e
rayray2002 <rayray2002.huang@gmail.com>
2022-03-19 16:30:05 +0800

    plot

--- plot.py
+++ plot.py
@@ -8,6 +8,6 @@ for user in users:
     df = pd.read_csv(f'data/{user}_out.csv')
 
     cols = df.columns
-    df.plot.line(x='timestamp', subplots=True, figsize=(24,8))
+    df.interpolate('linear').plot.line(x='timestamp', subplots=True, figsize=(24,8))
     plt.title(user)
     plt.savefig(f'data/plot/{user}_out.png')
